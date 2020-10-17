12607ed25624f9c74d7a0b795df66b74f960170c
jieqiang.li <147390612@qq.com>
2020-10-17 22:39:37 +0800

    fix logic problem

--- findPic.py
+++ findPic.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 ,win32gui ,pyautogui
 import capturebyhwnd 
 
 path="G:/python_job/screenshot/"
@@ -12,27 +12,35 @@ def find_pic_in_template(target, template):
 	result = cv2.matchTemplate(tar,temp,cv2.TM_SQDIFF_NORMED)
 	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 	#如果匹配度小于99%,就认为没有找到
-	if min_val >0.01:
-		return None
+	#if min_val >0.01:
+		#return None
 	
 	print(str(min_val))
 	#计算要点击的坐标
 	x=min_loc[0] + tar_w//3
 	y=min_loc[1] + tar_h//3
-	return (x,y)
+	return x,y
 	
 def mouse_click_abs(hwnd,x,y):
 	left,top,right,bot = win32gui.GetWindowRect(hwnd)
 	nx = x + left + d
 	ny = y + top + d
-	pyautogui.click(nx,ny)
+	for i in range(10):
+		pyautogui.click(nx,ny)
+		print("click at " + str(nx) + "," + str(ny))
 	
 def debug():
-	hwnd = capturebyhwnd.gethwndbyName("思羽")
+	hwnd = capturebyhwnd.gethwndbyName("安静的奇女子")
 	capturebyhwnd.pil_capture(path+"template.png",hwnd)
-	capturebyhwnd().pil_capture_by_coordinate(path+"target.png",400,400,20)
-	pos = find_pic_in_template(target,template)
-	mouse_click_abs(pos)
+	#capturebyhwnd().pil_capture_by_coordinate(path+"target.png",400,400,20)
+	x,y= find_pic_in_template("a","template")
+	mouse_click_abs(hwnd,x,y)
+	#x,y  = find_pic_in_template("b","template")
+	#mouse_click_abs(hwnd,x,y)
+	#x,y  = find_pic_in_template("c","template")
+	#mouse_click_abs(hwnd,x,y)
+	#x,y  = find_pic_in_template("d","template")
+	#mouse_click_abs(hwnd,x,y)
 	
 if __name__ == "__main__":
 	debug()	
